b7c510179a85304dc6d2d1b17ac267178b46a9ca
Aman Shrestha <ashrest2@u.rochester.edu>
2019-09-27 23:45:50 -0400

    Initial Commit

---
+++ FallPuzzle/Model.swift
@@ -0,0 +1,231 @@
+//
+//  Model.swift
+//  FallPuzzle
+//
+//  Created by Aman Shrestha on 9/27/19.
+//  Copyright © 2019 Aman Shrestha. All rights reserved.
+//
+
+import Foundation
+
+class Puzzle{
+    
+    
+    var puzzleBoxes = Array(repeating: Array(repeating: 0, count: 3), count: 3)
+    
+    var checking = Array(repeating: -1, count:9)
+    
+    func initializeArray(){
+        
+        var counter = 0
+        
+        self.checking = makeList(9)
+        
+        self.checking = [1,2,3,4,5,6,7,9,8]
+        
+        for i in 0...puzzleBoxes.count-1{
+            for j in 0...puzzleBoxes[0].count-1{
+                puzzleBoxes[i][j] = checking[counter]
+                counter = counter + 1
+            }
+        }
+        
+    }
+    
+    func makeList(_ n: Int) -> [Int] {
+        var toReturn = [Int](repeating: -1, count: n)
+        var numbersAdded = 0
+        
+        while(numbersAdded < n){
+            var number = Int.random(in: 1 ... n)
+            while(toReturn.contains(number)){
+                number = Int.random(in: 1 ... n)
+            }
+            toReturn[numbersAdded] = number
+            numbersAdded = numbersAdded + 1
+        }
+        
+        return toReturn
+    }
+    
+    func ifWon()->Bool{
+        var counter = 1
+        for i in 0...puzzleBoxes.count-1{
+            for j in 0...puzzleBoxes[0].count-1{
+                if(puzzleBoxes[i][j] != counter){return false}
+                counter = counter + 1
+            }
+        }
+        return true
+        
+    }
+    
+    
+    
+    func checkIfMovementPossible(tag: Int)->Bool{
+        
+        var row = -1
+        var column = -1
+        
+       
+        
+        for i in 0...puzzleBoxes.count-1{
+            for j in 0...puzzleBoxes[0].count-1{
+                if(puzzleBoxes[i][j] == tag){
+                    row = i
+                    column = j
+                    break
+                }
+            }
+        }
+        if(row == -1 || column == -1){return false}
+        if(row == 0){
+            if(column == 0){
+                if(puzzleBoxes[row+1][column] == 9){
+                    self.puzzleBoxes = swapNumbersInArray(row1: row, column1: column, row2: row+1, column2: column)
+                    return true
+                }
+                if(puzzleBoxes[row][column+1] == 9){
+                    self.puzzleBoxes = swapNumbersInArray(row1: row, column1: column, row2: row, column2: column+1)
+                    return true
+                }
+            }
+            if(column == 1){
+                if(puzzleBoxes[row+1][column] == 9){
+                    self.puzzleBoxes = swapNumbersInArray(row1: row, column1: column, row2: row+1, column2: column)
+                    return true
+                }
+                if(puzzleBoxes[row][column+1] == 9){
+                    self.puzzleBoxes = swapNumbersInArray(row1: row, column1: column, row2: row, column2: column+1)
+                    return true
+                }
+                if(puzzleBoxes[row][column-1] == 9){
+                    self.puzzleBoxes = swapNumbersInArray(row1: row, column1: column, row2: row, column2: column-1)
+                    return true
+                }
+            }
+            if(column == 2){
+                if(puzzleBoxes[row+1][column] == 9){
+                    self.puzzleBoxes = swapNumbersInArray(row1: row, column1: column, row2: row+1, column2: column)
+                    return true
+                }
+                if(puzzleBoxes[row][column-1] == 9){
+                    self.puzzleBoxes = swapNumbersInArray(row1: row, column1: column, row2: row, column2: column-1)
+                    return true
+                }
+            }
+        }
+        else if(row == 1){
+            if(column == 0){
+                if(puzzleBoxes[row+1][column] == 9){
+                    self.puzzleBoxes = swapNumbersInArray(row1: row, column1: column, row2: row+1, column2: column)
+                    return true
+                }
+                if(puzzleBoxes[row][column+1] == 9){
+                    self.puzzleBoxes = swapNumbersInArray(row1: row, column1: column, row2: row, column2: column+1)
+                    return true
+                }
+                if(puzzleBoxes[row-1][column] == 9){
+                    self.puzzleBoxes = swapNumbersInArray(row1: row, column1: column, row2: row-1, column2: column)
+                    return true
+                }
+            }
+            if(column == 1){
+                if(puzzleBoxes[row+1][column] == 9){
+                    self.puzzleBoxes = swapNumbersInArray(row1: row, column1: column, row2: row+1, column2: column)
+                    return true
+                }
+                if(puzzleBoxes[row][column+1] == 9){
+                    self.puzzleBoxes = swapNumbersInArray(row1: row, column1: column, row2: row, column2: column+1)
+                    return true
+                }
+                if(puzzleBoxes[row][column-1] == 9){
+                    self.puzzleBoxes = swapNumbersInArray(row1: row, column1: column, row2: row, column2: column-1)
+                    return true
+                }
+                if(puzzleBoxes[row-1][column] == 9){
+                    self.puzzleBoxes = swapNumbersInArray(row1: row, column1: column, row2: row-1, column2: column)
+                    return true
+                }
+            }
+            if(column == 2){
+                if(puzzleBoxes[row+1][column] == 9){
+                    self.puzzleBoxes = swapNumbersInArray(row1: row, column1: column, row2: row+1, column2: column)
+                  
+                    
+                    return true
+                }
+                if(puzzleBoxes[row][column-1] == 9){
+                    self.puzzleBoxes = swapNumbersInArray(row1: row, column1: column, row2: row, column2: column-1)
+                    return true
+                }
+                if(puzzleBoxes[row-1][column] == 9){
+                    self.puzzleBoxes = swapNumbersInArray(row1: row, column1: column, row2: row-1, column2: column)
+                    return true
+                }
+            }
+        }
+        else if(row == 2){
+            if(column == 0){
+                if(puzzleBoxes[row-1][column] == 9){
+                    self.puzzleBoxes = swapNumbersInArray(row1: row, column1: column, row2: row-1, column2: column)
+                    return true
+                }
+                if(puzzleBoxes[row][column+1] == 9){
+                    self.puzzleBoxes = swapNumbersInArray(row1: row, column1: column, row2: row, column2: column+1)
+                    return true
+                }
+            }
+            if(column == 1){
+                if(puzzleBoxes[row-1][column] == 9){
+                    self.puzzleBoxes = swapNumbersInArray(row1: row, column1: column, row2: row-1, column2: column)
+                    return true
+                }
+                if(puzzleBoxes[row][column+1] == 9){
+                    self.puzzleBoxes = swapNumbersInArray(row1: row, column1: column, row2: row, column2: column+1)
+                    return true
+                }
+                if(puzzleBoxes[row][column-1] == 9){
+                    self.puzzleBoxes = swapNumbersInArray(row1: row, column1: column, row2: row, column2: column-1)
+                    return true
+                }
+            }
+            if(column == 2){
+                if(puzzleBoxes[row-1][column] == 9){
+                    self.puzzleBoxes = swapNumbersInArray(row1: row, column1: column, row2: row-1, column2: column)
+                    return true
+                }
+                if(puzzleBoxes[row][column-1] == 9){
+                    self.puzzleBoxes = swapNumbersInArray(row1: row, column1: column, row2: row, column2: column-1)
+                    return true
+                }
+            }
+        }
+        
+        
+        
+        return false
+    }
+    
+   
+    func swapNumbersInArray(row1 : Int,column1 : Int, row2: Int,column2 : Int)-> [[Int]]{
+        
+        var temp : Int
+        temp = self.puzzleBoxes[row1][column1]
+        self.puzzleBoxes[row1][column1] = self.puzzleBoxes[row2][column2]
+        self.puzzleBoxes[row2][column2] = temp
+        return self.puzzleBoxes
+        
+    }
+    
+    //        for i in 0...puzzleBoxes.count-1{
+    //            for j in 0...puzzleBoxes[0].count-1{
+    //                print(puzzleBoxes[i][j], terminator:"")
+    //            }
+    //            print()
+    //        }
+
+    
+    
+    
+}
---
+++ FallPuzzle/ViewController.swift
@@ -0,0 +1,139 @@
+//
+//  ViewController.swift
+//  FallPuzzle
+//
+//  Created by Aman Shrestha on 9/27/19.
+//  Copyright © 2019 Aman Shrestha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var puzzleObject = Puzzle()
+    var imagesHere = [UIImage]()
+    
+    @IBOutlet weak var imageButton1: UIButton!
+    @IBOutlet weak var imageButton2: UIButton!
+    @IBOutlet weak var imageButton3: UIButton!
+    @IBOutlet weak var imageButton4: UIButton!
+    @IBOutlet weak var imageButton5: UIButton!
+    @IBOutlet weak var imageButton6: UIButton!
+    @IBOutlet weak var imageButton7: UIButton!
+    @IBOutlet weak var imageButton8: UIButton!
+    @IBOutlet weak var imageButton9: UIButton!
+    
+    
+    @IBOutlet weak var congratsMessage: UILabel!
+    
+    @IBAction func restartButton(_ sender: Any) {
+        
+        imageButton1.tag = 1
+        imageButton1.setImage(UIImage(named: "Image1"), for: .normal)
+        imageButton2.tag = 2
+        imageButton2.setImage(UIImage(named: "Image2"), for: .normal)
+        imageButton3.tag = 3
+        imageButton3.setImage(UIImage(named: "Image3"), for: .normal)
+        imageButton4.tag = 4
+        imageButton4.setImage(UIImage(named: "Image4"), for: .normal)
+        imageButton5.tag = 5
+        imageButton5.setImage(UIImage(named: "Image5"), for: .normal)
+        imageButton6.tag = 6
+        imageButton6.setImage(UIImage(named: "Image6"), for: .normal)
+        imageButton7.tag = 7
+        imageButton7.setImage(UIImage(named: "Image7"), for: .normal)
+        imageButton8.tag = 8
+        imageButton8.setImage(UIImage(named: "Image8"), for: .normal)
+        imageButton9.tag = 9
+        imageButton9.setImage(UIImage(named: "Image9"), for: .normal)
+
+        viewDidLoad()
+        
+    }
+    
+    @IBAction func button1(_ sender: UIButton) {
+        
+        
+        //Change the array every time the button has been pressed
+        
+        var movePossible = false
+        
+        movePossible = puzzleObject.checkIfMovementPossible(tag: sender.tag)
+        
+        let emptyButton = self.view.viewWithTag(9) as? UIButton
+        
+        var buttonImage:UIImage
+        var emptyButtonImage:UIImage
+
+        var temp: Int
+        if(movePossible){
+            buttonImage = sender.imageView!.image!
+            emptyButtonImage = (emptyButton?.imageView!.image!)!
+            
+            
+            sender.setImage(emptyButtonImage, for: .normal)
+            emptyButton?.setImage(buttonImage, for: .normal)
+            
+            temp = sender.tag
+            sender.tag = emptyButton!.tag
+            emptyButton?.tag = temp
+        }
+        
+        let gameEnd = puzzleObject.ifWon()
+        
+        if(gameEnd){
+            congratsMessage.isHidden = false
+        }else{
+            congratsMessage.isHidden = true
+        }
+
+        
+        
+
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        congratsMessage.isHidden = true
+        randomizingStartingPosition()
+    }
+    
+    func randomizingStartingPosition(){
+        
+
+        var fileName = "Image"
+        for i in 1...9{
+            fileName = "Image\(i)"
+            imagesHere.append(UIImage(named: fileName)!)
+        }
+        
+       
+
+
+        puzzleObject.initializeArray()
+        
+        
+        var helperCounter = 0
+        for i in 1...9{
+            let foundButton = self.view.viewWithTag(i) as? UIButton
+            foundButton!.tag = puzzleObject.checking[helperCounter] * 10
+            foundButton?.setImage(imagesHere[puzzleObject.checking[helperCounter]-1], for: .normal)
+            helperCounter = helperCounter + 1
+        }
+        
+
+        for i in stride(from:10, to:91, by:10){
+            let foundButton = self.view.viewWithTag(i) as? UIButton
+            let tagFound = foundButton?.tag
+            foundButton!.tag = tagFound! / 10
+        }
+
+        
+        
+        
+        
+    }
+
+
+}
+
